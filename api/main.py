0acabed9588be23f2bd9062ba058d6755d97cb2e
daphnehlj <daphnehlj25@gmail.com>
2026-07-03 00:08:12 -0700

    implement competitors citations to competitors tab

--- api/main.py
+++ api/main.py
@@ -10,6 +10,7 @@ from api.queries import (
     get_top_positives,
     get_top_concerns,
     get_top_competitors_by_school,
+    get_competitor_citations,
     get_citations,
     get_summary,
     get_competitor_win_rate,
@@ -108,6 +109,10 @@ def top_competitors_by_school(days: int = None, school: str = None):
 @app.get("/api/competitor-win-rate")
 def competitor_win_rate(days: int = None, school: str = None):
     return get_competitor_win_rate(days, school)
+
+@app.get("/api/competitor-citations")
+def competitor_citations(competitor: str, days: int = None, school: str = None):
+    return get_competitor_citations(competitor, days, school)
 
 
 @app.get("/api/qc-citations")
